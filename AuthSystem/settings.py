19f504d4bb2a70bb200fa1c84aea494c179ea930
siddarthacharya <siddarthacharya51@gmail.com>
2025-08-09 21:02:01 +0530

    Updated Django settings.py with new code

--- AuthSystem/settings.py
+++ AuthSystem/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 
+import pymysql
+pymysql.install_as_MySQLdb()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -78,10 +81,18 @@ WSGI_APPLICATION = 'AuthSystem.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'myproject_db',
+        'USER': 'root',       # Default XAMPP MySQL username
+        'PASSWORD': '',       # Leave empty unless you set a password
+        'HOST': '127.0.0.1',  # Or 'localhost'
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
     }
 }
+
 
 
 # Password validation
